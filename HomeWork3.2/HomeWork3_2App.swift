e927d3a64935a8bf51e874662eab2182ed698df9
MichaelPNZ <85676466+MichaelPNZ@users.noreply.github.com>
2022-01-24 23:27:00 +0300

    Initial Commit

---
+++ HomeWork3.2/HomeWork3_2App.swift
@@ -0,0 +1,17 @@
+//
+//  HomeWork3_2App.swift
+//  HomeWork3.2
+//
+//  Created by Михаил Позялов on 24.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct HomeWork3_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
